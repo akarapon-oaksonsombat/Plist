4598929a42e7e69bd0304503c988f2dd185f5a6f
Akarapon Oaksonsombat <47691236+akarapon-oaksonsombat@users.noreply.github.com>
2020-09-15 23:29:26 +0700

    Update pubspec.lock

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterexam/components/app.dart';
+import 'file:///C:/Users/akara/Documents/GitHub/Plist/lib/app.dart';
 
 import 'package:flutterexam/main.dart';
 
